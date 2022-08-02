b6bb4445c922b2a465180dbe36f6f7c967124531
dingbo <dingbo8128@163.com>
2022-08-02 08:46:22 +0800

    test

--- driver-tdengine/src/main/java/io/openmessaging/benchmark/driver/tdengine/TDengineProducer.java
+++ driver-tdengine/src/main/java/io/openmessaging/benchmark/driver/tdengine/TDengineProducer.java
@@ -105,7 +105,7 @@ public class TDengineProducer {
                 }
 
             }
-        } catch (SQLException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         } finally {
             try {
